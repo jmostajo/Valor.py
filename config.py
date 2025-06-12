c043cd0070210dcc54a3cd8a61d6143a6b0376e4
Juan Jose Mostajo Leon <juanjosemostajoleon@Juans-MacBook-Air.local>
2025-06-12 15:31:16 -0500

    Agrega config.py con redirect_uri corregido

---
+++ config.py
@@ -0,0 +1,5 @@
+client_id = "df0b1287-f8f7-4c89-bc34-bab9abadead8"
+tenant_id = "4322eb3a-d8ab-45a9-865b-475f3427324f"
+client_secret = "mbg8Q~iHwcHtsbJBu7v8q42oMPHfmZHR4QezHaL4"
+redirect_uri = "https://valorpy-o2qwwnyu4sq5j2j47cknce.streamlit.app/"
+
